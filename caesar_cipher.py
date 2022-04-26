6463aa56dc4b248960fc3ca816ca3121179666b5
p22jdiao <p22jdiao@fairfieldprep.org>
2022-04-26 09:47:54 -0400

    com

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -16,26 +16,26 @@ def shift_line(line, dict_key):
     new_line = ""
     for x in line:
         try:
-            if x.isupper() == True:
-                new_line += dict_key[x.upper()]
-            else:
+            if x.isupper() == True: # Because the cipher_key() function returns only the capital letters
+                new_line += dict_key[x.upper()] # If the original letter is a upper case letter then just use the corresponding key
+            else: # If the original letter is a lowercase letter, then change the replacing key into a lowercase letter
                 new_line += dict_key[x.upper()].lower()
-        except KeyError:
-            new_line += x
+        except KeyError: # The key function does not contain any symbols, and there is no need to replace a symbol
+            new_line += x # So just simply use the original symbol, which is x
     return new_line
 
 
 def encrypt_message(filename, dict_key):
-    old_file = open(filename,"r")
-    new_file = open("new_file.txt","a")
+    old_file = open(filename,"r") # Use reading mode to open the original file
+    new_file = open("new_file.txt","a") # Use append mode for the moderating one.
 
     while True:
-        o_line = old_file.readline()
+        o_line = old_file.readline() # Go through the original file line by line
 
-        n_line = shift_line(o_line,dict_key)
-        new_file.write(n_line+"\n")
+        n_line = shift_line(o_line,dict_key) # Create the shifted version of the line
+        new_file.write(n_line+"\n") # Add the shifted line into the new file
 
-        if len(o_line) == 0:
+        if len(o_line) == 0: # Once the .readline() reaches the end, the length of the line will be zero, then break the while loop
             break
     
     old_file.close()
